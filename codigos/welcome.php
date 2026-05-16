4bf1e4edf89fb6a928de1f3589a2d1e934028890
jv-sanjos23 <eurogunz.90@gmail.com>
2026-05-16 19:52:12 -0300

    Create welcome.php

---
+++ codigos/welcome.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Bem-vindo</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body class="welcome-body">
+
+    <div class="welcome-container">
+
+        <!-- TOPO -->
+
+        <div class="welcome-top">
+
+            <h1>
+                SEJA BEM-VINDO(A)!
+            </h1>
+
+            <p>
+                A TASKS TE AJUDA A ORGANIZAR SUAS TAREFAS!
+            </p>
+
+            <!-- IMAGEM -->
+
+            <div class="welcome-img">
+
+                <!-- troque pela sua imagem -->
+                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png">
+
+            </div>
+
+        </div>
+
+        <!-- PARTE DE BAIXO -->
+
+        <div class="welcome-bottom">
+
+            <h2>
+                TASKS!
+            </h2>
+
+            <a href="register.php" class="btn-welcome">
+                CADASTRE-SE
+            </a>
+
+            <span class="ou">
+                OU
+            </span>
+
+            <a href="login.php" class="btn-welcome">
+                ENTRE
+            </a>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
