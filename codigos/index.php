a5ba0aa27adcd3b1f745ede14b91985ef707abc9
jv-sanjos23 <eurogunz.90@gmail.com>
2026-05-23 21:13:33 -0300

    Update index.php

--- codigos/index.php
+++ codigos/index.php
@@ -8,6 +8,11 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = $_SESSION['user_id'];
 
+$config = $conn->query("
+SELECT * FROM configuracoes
+WHERE usuario_id = $user_id
+")->fetch_assoc();
+
 /* CONCLUIR */
 if (isset($_GET['concluir'])) {
     $id = intval($_GET['concluir']);
@@ -21,10 +26,33 @@ if (isset($_GET['excluir'])) {
 }
 
 /* LISTAR */
-$tarefas = $conn->query("SELECT * FROM tarefas WHERE usuario_id = $user_id ORDER BY horario ASC");
+$sql = "
+SELECT * FROM tarefas
+WHERE usuario_id = $user_id
+ORDER BY horario ASC
+";
 
-$data = date("d \\d\\e F, Y");
-$dia = strtoupper(strftime("%A"));
+if($config['limite_visual']){
+
+    $limite = intval($config['quantidade_tarefas']);
+
+    $sql .= " LIMIT $limite";
+}
+
+$tarefas = $conn->query("
+    SELECT * FROM tarefas 
+    WHERE usuario_id = $user_id 
+    AND concluida = 0
+    ORDER BY horario ASC
+");
+
+date_default_timezone_set('America/Sao_Paulo');
+
+setlocale(LC_TIME, 'pt_BR.UTF-8', 'pt_BR', 'Portuguese_Brazil');
+
+$dia = strtoupper(strftime('%A'));
+
+$data = strftime('%d de %B, %Y.');
 ?>
 
 <!DOCTYPE html>
@@ -45,12 +73,28 @@ $dia = strtoupper(strftime("%A"));
         <h1><?= $dia ?></h1>
         <p><?= $data ?></p>
     </div>
-    <div class="perfil"></div>
+    <div class="perfil-box">
+
+    <span class="nome-user">
+        <?= $_SESSION['nome'] ?>
+    </span>
+
+    <img src="images/usuario.png" class="foto-user">
+
+</div>
 </header>
 
 <section class="tarefas">
 <?php while($t = $tarefas->fetch_assoc()): ?>
-    <div class="card <?= ($t['prioridade'] == 'importante') ? 'importante' : '' ?>">
+    <div 
+class="card <?= ($t['prioridade'] == 'importante') ? 'importante' : '' ?>"
+
+data-titulo="<?= $t['titulo'] ?>"
+data-data="<?= $t['data'] ?>"
+data-horario="<?= $t['horario'] ?>"
+data-prioridade="<?= $t['prioridade'] ?>"
+data-descricao="<?= $t['descricao'] ?>"
+>
         
         <h2><?= $t['titulo'] ?></h2>
 
@@ -64,12 +108,12 @@ $dia = strtoupper(strftime("%A"));
         <?php endif; ?>
 
         <div class="acoes">
-            <a href="?concluir=<?= $t['id'] ?>">
-                <button class="ok">✔</button>
+            <a href="?concluir=<?= $t['id'] ?>" onclick="event.stopPropagation()">
+                <button class="ok efeito-btn">✔</button>
             </a>
 
-            <a href="?excluir=<?= $t['id'] ?>">
-                <button class="cancel">✖</button>
+            <a href="?excluir=<?= $t['id'] ?>" onclick="event.stopPropagation()">
+                <button class="cancel efeito-btn">✖</button>
             </a>
         </div>
 
@@ -90,12 +134,110 @@ $dia = strtoupper(strftime("%A"));
     
     <!-- BOTÃO QUE VAI PRA OUTRA TELA -->
     <a href="nova_tarefa.php">
-        <button class="add">+</button>
+        <button class="add" id="btnAdd">+</button>
     </a>
 
-    <span>⏱</span>
-    <span>⚙</span>
+    <a href="pomodoro.php" style="color: black; text-decoration: none;"><span>⏱</span></a>
+    <a href="configuracoes.php" style="color: black; text-decoration: none;"><span >⚙</span></a>
 </nav>
 
+<!-- MODAL -->
+
+<div class="modal" id="modal">
+
+    <div class="modal-box">
+
+        <button class="fechar" id="fecharModal">
+            ✖
+        </button>
+
+        <h2 id="modalTitulo"></h2>
+
+        <p>
+            <b>Data:</b>
+            <span id="modalData"></span>
+        </p>
+
+        <p>
+            <b>Horário:</b>
+            <span id="modalHorario"></span>
+        </p>
+
+        <p>
+            <b>Prioridade:</b>
+            <span id="modalPrioridade"></span>
+        </p>
+
+        <p>
+            <b>Descrição:</b>
+            <span id="modalDescricao"></span>
+        </p>
+
+    </div>
+
+</div>
+
+<script>
+
+const cards = document.querySelectorAll(".card");
+
+const modal = document.getElementById("modal");
+
+const fechar = document.getElementById("fecharModal");
+
+cards.forEach(function(card){
+
+    card.addEventListener("click", function(){
+
+        document.getElementById("modalTitulo").innerText =
+        card.dataset.titulo;
+
+        document.getElementById("modalData").innerText =
+        card.dataset.data;
+
+        document.getElementById("modalHorario").innerText =
+        card.dataset.horario;
+
+        document.getElementById("modalPrioridade").innerText =
+        card.dataset.prioridade;
+
+        document.getElementById("modalDescricao").innerText =
+        card.dataset.descricao;
+
+        modal.style.display = "flex";
+
+    });
+
+});
+
+fechar.addEventListener("click", function(){
+
+    modal.style.display = "none";
+
+});
+
+</script>
+
 </body>
+
+<script>
+const botoes = document.querySelectorAll(".efeito-btn");
+botoes.forEach(function(botao){
+    botao.addEventListener("mouseover", function(){
+        botao.style.transform = "scale(1.2)";
+    });
+    botao.addEventListener("mouseout", function(){
+        botao.style.transform = "scale(1)";
+    });
+});    
+const botao = document.getElementById("btnAdd");
+botao.addEventListener("mouseover", function(){
+    botao.style.transform = "translateX(-50%) scale(1.2)";
+});
+botao.addEventListener("mouseout", function(){
+    botao.style.transform = "translateX(-50%) scale(1)";
+
+});
+</script>
+
 </html>
