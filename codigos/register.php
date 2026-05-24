7d2096687b5137d56d7f8f52c674fbc835a2823f
jv-sanjos23 <eurogunz.90@gmail.com>
2026-05-24 17:49:23 -0300

    Update register.php

--- codigos/register.php
+++ codigos/register.php
@@ -47,6 +47,8 @@ if ($_POST) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Cadastro</title>
 <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body class="cadastro-body">
 <div class="cadastro-box">
