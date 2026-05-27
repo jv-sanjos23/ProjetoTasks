1aa89f69ce4368a20f8e8bff3a98075e0bc32587
Biel <140564192+Biel1512@users.noreply.github.com>
2026-05-27 08:20:16 -0300

    Add files via upload
    Tasks 100% atualizado

---
+++ Tasks/config.php
@@ -0,0 +1,13 @@
+    <?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "tarefas_app";
+
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if($conn->connect_error){
+        die("Erro na conexão: " . $conn->connect_error);
+    }
+    ?>
---
+++ Tasks/recuperar_senha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Recuperar Senha</title>
+</head>
+<body>
+
+<h2>Recuperar Senha</h2>
+
+<form action="recuperar.php" method="POST">
+
+    <input type="email"
+           name="email"
+           placeholder="Digite seu email"
+           required>
+
+    <button type="submit">
+
+        Enviar link
+
+    </button>
+
+</form>
+
+</body>
+</html>
